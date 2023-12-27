b0120e1b7473be17b486eaeb68493d5a18abd219
ujjwalpoudel2003 <upoudel1@my.centennialcollege.ca>
2023-12-27 02:06:48 -0500

    Logo set in navigation

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -57,7 +57,7 @@ export default function Document() {
       <Head>
         <script dangerouslySetInnerHTML={{ __html: themeScript }} />
       </Head>
-      <body className="bg-white dark:bg-slate-900">
+      <body className="bg-white dark:bg-[#1c0b3a]">
         <Main />
         <NextScript />
       </body>
